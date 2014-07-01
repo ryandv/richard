d01bd18cf1517986e6aee3f7572d89db3ba34695
robert dooley <robertofdooley@gmail.com>
2014-07-01 12:14:13 -0400

    removes unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,6 @@ Richard::Application.routes.draw do
     end
   end
 
-  match 'start_waiting/:id' => 'users#start_waiting'
-  match 'stop_waiting/:id' => 'users#stop_waiting'
-  match 'run_gorgon/:id' => 'users#run_gorgon'
-  match 'finish_running/:id' => 'users#finish_running'
-
   root :to => 'queue_transactions#index'
   match 'users_json' => 'users#users_json'
 end
