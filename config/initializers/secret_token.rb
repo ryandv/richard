aad05ca2115b83c39b68570ba77d988a3b9ad15e
robert dooley <robertd@nulogy.com>
2014-07-20 10:00:59 -0400

    migrate config files to rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Richard::Application.config.secret_token = 'd8e71107589e6a29e0842f15a998b518d6c685bbc349747d95bf25633af584cb4624b6a91f399e844ec4df1dbe52605028f88c660d85a4c70af0245dc7169ad1'
+
+Richard::Application.config.secret_key_base = '40725788a710889a9f4b7c8481b83b730f8ea0b632fa400fad3a278e182b50aea93e5ea836579b1d7df3b4ebe9a435e1d33f6d29ff2f09741934b02ec0ea0e17'
