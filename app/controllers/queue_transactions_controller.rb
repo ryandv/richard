6349e033eafcd1d97aa443f385185c8cc941fb02
robert dooley <robertd@nulogy.com>
2014-07-20 10:08:23 -0400

    removes unnecessary strong parameters

--- app/controllers/queue_transactions_controller.rb
+++ app/controllers/queue_transactions_controller.rb
@@ -101,8 +101,4 @@ private
   def load_queue_transaction
     @queue_transaction = QueueTransaction.find(params[:id])
   end
-
-  def queue_transaction_params
-    params.require(:queue_transaction).permit(:waiting_start_at, :pending_start_at, :running_start_at, :user_id, :finished_at, :cancelled_at, :is_complete, :force_release_at)
-  end
 end
