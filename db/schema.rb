9a13bc0f280eb4510476e1e6b8c5c7f19f5e99a4
robert dooley <robertd@nulogy.com>
2014-07-25 13:37:30 -0400

    updates schema rails 4

--- db/schema.rb
+++ db/schema.rb
@@ -9,43 +9,46 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140706161841) do
+ActiveRecord::Schema.define(version: 20140706161841) do
 
-  create_table "gorgon", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "gorgon", force: true do |t|
     t.integer "status"
     t.integer "user_id"
   end
 
-  create_table "queue_transactions", :force => true do |t|
+  create_table "queue_transactions", force: true do |t|
     t.integer  "user_id"
     t.datetime "waiting_start_at"
     t.datetime "pending_start_at"
     t.datetime "running_start_at"
     t.datetime "finished_at"
     t.datetime "cancelled_at"
-    t.boolean  "is_complete",      :default => false
-    t.datetime "created_at",                          :null => false
-    t.datetime "updated_at",                          :null => false
+    t.boolean  "is_complete",      default: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.datetime "force_release_at"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",               :default => "", :null => false
-    t.string   "encrypted_password",  :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: "", null: false
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",       :default => 0,  :null => false
+    t.integer  "sign_in_count",       default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          :null => false
-    t.datetime "updated_at",                          :null => false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.string   "name"
     t.string   "avatar_url"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
